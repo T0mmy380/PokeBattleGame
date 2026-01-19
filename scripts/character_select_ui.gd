18837fe8c7f28ebbfbebf7bc9d88093acbc0623d
Tommy Lev <tommy.levasseur@icloud.com>
2026-01-19 17:05:33 +0000

    created character selector

---
+++ scripts/character_select_ui.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var character: Node2D = $"../Pokemon"
+@onready var popup: Window = $Window
+@onready var text_feild: LineEdit = $"Window/Text Feild"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_curent_character()
+
+func _on_select_button_pressed() -> void:
+	set_curent_character()
+	popup.hide()
+
+func get_character_name() -> String:
+	return text_feild.text
+
+func set_curent_character() -> void:
+	character.set_character_by_name(get_character_name())
